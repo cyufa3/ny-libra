5cbdaafc7c770d25aff4c6c5924443b582cbbf11
CYUZUZO Fabrice <fabricecyufa@gmail.com>
2025-11-27 00:50:22 +0200

    Your new update message

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,10 @@ app.get("/api/books/count", async (req, res) => {
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
